ed4e6acc4928681847723729faf6b9cae37eb257
Sarah <sweetiejeremy@hotmail.com>
2017-03-15 15:28:52 -0700

    Added description

--- Linked_Lists_3.js
+++ Linked_Lists_3.js
@@ -1,4 +1,6 @@
-deleteMidNode: function(node) {
+// Deletes middle node with access only to that node
+
+function deleteMidNode(node) {
 		var n = this.findMidNode(node, this._root);
 		if(n == null || n.pointer == null){
 			return false;
