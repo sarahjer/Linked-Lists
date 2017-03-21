e6ab49a0735b6ee8cc758f6b3c49441bd0361bd3
Sarah <sweetiejeremy@hotmail.com>
2017-03-21 15:17:35 -0700

    Added description

--- Linked_Lists_4.js
+++ Linked_Lists_4.js
@@ -1,3 +1,6 @@
+// Partitions a linked list around a value p such that all nodes greater than value p are to its right and all values
+// lesser are to its left. The partition value need not appear between the left and right partitions.
+
 function partition(node, p) {
 	var head = node;
 	var tail = node;
