ed368b372a8030323d29fe0d0ad409d304fd9874
Sarah <sweetiejeremy@hotmail.com>
2017-03-23 16:18:01 -0700

    Basic function

---
+++ Linked_Lists_5.js
@@ -0,0 +1,23 @@
+function linkedList(n){
+	this._root = null;
+}
+
+function addNode(n){
+	var node = {};
+	node.value = n;
+	node.pointer = null;
+	var curreent;
+	if()
+
+}
+
+function sumLists(l1, l2){
+	var node = l1.root;
+	var node1 = l2.root;
+while(node != null){
+
+}
+}
+
+
+var ll = new linkedList();
