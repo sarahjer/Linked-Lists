bc7699d6c70f2c7162fdd3f1fa2e93c5c83f5506
Sarah <sweetiejeremy@hotmail.com>
2017-03-28 15:53:27 -0700

    Added new method

--- Linked_Lists_5.js
+++ Linked_Lists_5.js
@@ -19,3 +19,19 @@ function sumLists(l1, l2){
 	}
 	return ll3;
 }
+
+function sumListsR(l1, l2) {
+	var current1 = l1._root;
+	var current2 = l2._root; 
+	var sum;
+	var carry = 0;
+	while(current1.pointer != null){
+		current1 = current1.pointer;
+		current2 = current2.pointer;
+	}
+	sum = current1.value + current2.value + carry;
+	carry = sum >= 10 ? 1 : 0; 
+	if(sum >= 10 ){
+		sum = sum % 10;
+	} 
+}
