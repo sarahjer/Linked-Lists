99436f8444640f127abc61514da37b9b09ef4bd6
Sarah <sweetiejeremy@hotmail.com>
2017-04-10 14:49:35 -0700

    Reverse & compare

--- Linked_Lists_6.js
+++ Linked_Lists_6.js
@@ -1,13 +1,26 @@
-function(l1, l2){
-	console.log("l1:" + l1.value);
-	console.log("l2:" + l2.value);
-	while(l1 != null && l2 != null){
-		if(l1.value == l2.value){
-			l1 = l1.pointer;
-			l2 = l2.pointer;
-		} else {
-			console.log(false);
-		}
-		console.log(true);
+//checks if a linked list is a palindrome
+
+function(l1){
+	var ll2 = new SingleLinkedList();
+	var ll1 = l1._root;
+	console.log(ll1);
+	while(ll1 != null){
+		ll2.add(ll1.value);
+		ll1 = ll1.pointer;
 	}
-}		
+	this.reverse(l1._root);
+	console.log(ll2._root);
+	console.log(ll._root);
+	var l11 = l1._root;
+	var l22 = ll2._root;
+	while(l11 != null && l22 != null){
+		if(l11.value == l22.value){
+			l11 = l11.pointer;
+			l22 = l22.pointer;
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+} 				
